fb9d5707744587dc93ef16b09a01fc065e2814e8
aaesalamanca <aaesalamanca@gmail.com>
2024-12-11 18:17:21 +0100

    refactor: Reduce the size of conversion rates dictionary & use metres as base unit

--- ScaleSwiftHWS/ContentView.swift
+++ ScaleSwiftHWS/ContentView.swift
@@ -15,14 +15,20 @@ struct ContentView: View {
     @State private var fromUnit = "m"
 
     private let conversionRates = [
-        "m-km": 0.001, "m-ft": 3.28084, "m-yd": 1.09361, "m-mi": 0.000621,
-        "km-m": 1000, "km-ft": 3280.84, "km-yd": 1093.61, "km-mi": 0.621,
-        "ft-m": 0.3048, "ft-km": 0.000305, "ft-yd": 0.333333, "ft-mi": 0.000189,
-        "yd-m": 0.9144, "yd-km": 0.000914, "yd-fy": 3, "yd-mi": 0.000568,
-        "mi-m": 1609.34, "mi-km": 1.69034, "mi-ft": 5280, "mi-yd": 1760,
+        "m-m": 1, "m-km": 0.001, "m-ft": 3.28084, "m-yd": 1.09361,
+        "m-mi": 0.000621, "km-m": 1000, "ft-m": 0.3048, "yd-m": 0.9144,
+        "mi-m": 1609.34,
     ]
+
     private var toDistance: Double {
-        (fromDistance ?? 0) * (conversionRates["\(fromUnit)-\(toUnit)"] ?? 1)
+        guard fromUnit != toUnit else {
+            return fromDistance ?? 0
+        }
+
+        let meters =
+            (fromDistance ?? 0) * conversionRates["\(fromUnit)-m"]!
+
+        return meters * conversionRates["m-\(toUnit)"]!
     }
 
     @State private var toUnit = "m"
