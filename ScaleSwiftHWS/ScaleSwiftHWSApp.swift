a6bb687584e7d377154e00bae6904b73d414b572
aaesalamanca <aaesalamanca@gmail.com>
2024-12-11 13:21:48 +0100

    Initial Commit

---
+++ ScaleSwiftHWS/ScaleSwiftHWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScaleSwiftHWSApp.swift
+//  ScaleSwiftHWS
+//
+//  Created by Alejandro Antonio Estornell Salamanca on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ScaleSwiftHWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
