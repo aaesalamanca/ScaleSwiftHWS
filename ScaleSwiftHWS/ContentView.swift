3c22ddfbf7c9a5c84cc4e123a5d3894db6d87783
aaesalamanca <aaesalamanca@gmail.com>
2024-12-11 13:47:45 +0100

    feat: Type the input distance to calculate from

--- ScaleSwiftHWS/ContentView.swift
+++ ScaleSwiftHWS/ContentView.swift
@@ -8,17 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var fromDistance = 0.0
+    @FocusState private var fromDistanceIsFocused: Bool
+
     var body: some View {
         NavigationStack {
             Form {
                 Section("From") {
-
+                    TextField(
+                        "From distance", value: $fromDistance, format: .number
+                    )
+                    .keyboardType(.decimalPad)
+                    .focused($fromDistanceIsFocused)
                 }
                 Section("To") {
 
                 }
             }
             .navigationTitle("ScaleSwiftHWS")
+            .toolbar {
+                if fromDistanceIsFocused {
+                    Button("Done") {
+                        fromDistanceIsFocused.toggle()
+                    }
+                }
+            }
         }
     }
 }
