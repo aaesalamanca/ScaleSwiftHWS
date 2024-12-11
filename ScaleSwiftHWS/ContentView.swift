687d29bb6875a1c61d7a68680565258e2f911a24
aaesalamanca <aaesalamanca@gmail.com>
2024-12-11 16:06:08 +0100

    feat: Pick the unit measure to calculate from

--- ScaleSwiftHWS/ContentView.swift
+++ ScaleSwiftHWS/ContentView.swift
@@ -8,18 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var fromDistance = 0.0
+    @State private var fromDistance: Double?
     @FocusState private var fromDistanceIsFocused: Bool
+
+    private var units = ["m", "km", "ft", "yd", "mi"]
+    @State private var fromUnit = "m"
 
     var body: some View {
         NavigationStack {
             Form {
                 Section("From") {
-                    TextField(
-                        "From distance", value: $fromDistance, format: .number
-                    )
-                    .keyboardType(.decimalPad)
-                    .focused($fromDistanceIsFocused)
+                    HStack {
+                        TextField(
+                            "Distance", value: $fromDistance,
+                            format: .number
+                        )
+                        .keyboardType(.decimalPad)
+                        .focused($fromDistanceIsFocused)
+                        Picker("From unit", selection: $fromUnit) {
+                            ForEach(units, id: \.self) {
+                                Text($0)
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    }
+
                 }
                 Section("To") {
 
