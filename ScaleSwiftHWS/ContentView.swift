2f5d4e4b7b1c0e4fc4e9bbf853cdac8dbe868ed2
Alejandro Estornell <aaesalamanca@gmail.com>
2024-12-12 12:11:34 +0100

    feat: Convert between distance units (#1)
    * feat: Describe initial form layout
    
    * feat: Type the input distance to calculate from
    
    * feat: Pick the unit measure to calculate from
    
    * feat: Calculate & show the conversion between units
    
    * refactor: Reduce the size of conversion rates dictionary & use metres as base unit

--- ScaleSwiftHWS/ContentView.swift
+++ ScaleSwiftHWS/ContentView.swift
@@ -8,14 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var fromDistance: Double?
+    @FocusState private var fromDistanceIsFocused: Bool
+
+    private let units = ["m", "km", "ft", "yd", "mi"]
+    @State private var fromUnit = "m"
+
+    private let conversionRates = [
+        "m-m": 1, "m-km": 0.001, "m-ft": 3.28084, "m-yd": 1.09361,
+        "m-mi": 0.000621, "km-m": 1000, "ft-m": 0.3048, "yd-m": 0.9144,
+        "mi-m": 1609.34,
+    ]
+
+    private var toDistance: Double {
+        guard fromUnit != toUnit else {
+            return fromDistance ?? 0
         }
-        .padding()
+
+        let meters =
+            (fromDistance ?? 0) * conversionRates["\(fromUnit)-m"]!
+
+        return meters * conversionRates["m-\(toUnit)"]!
+    }
+
+    @State private var toUnit = "m"
+
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section("From") {
+                    TextField(
+                        "Distance", value: $fromDistance,
+                        format: .number
+                    )
+                    .keyboardType(.decimalPad)
+                    .focused($fromDistanceIsFocused)
+                    Picker("From unit", selection: $fromUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+
+                Section("To") {
+                    Text(toDistance, format: .number)
+                    Picker("To unit", selection: $toUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+            }
+            .navigationTitle("ScaleSwiftHWS")
+            .toolbar {
+                if fromDistanceIsFocused {
+                    Button("Done") {
+                        fromDistanceIsFocused.toggle()
+                    }
+                }
+            }
+        }
     }
 }
 
